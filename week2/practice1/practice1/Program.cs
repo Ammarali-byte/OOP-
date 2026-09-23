6375eee7c0ffab5c3147a39100e2d04b1c03a2f7
Ammarali-byte <ummaraliummar75@gmail.com>
2026-09-23 22:02:17 +0500

    practice

---
+++ week2/practice1/practice1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+
+        }
+    }
+}
